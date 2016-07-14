303c15af39339cf461f0c73ea945583b864e9a7a
bjv <jubo_1992@163.com>
2016-07-14 19:40:16 +0800

    update map stuff

--- dojoConfig.js
+++ dojoConfig.js
@@ -1,8 +1,26 @@
-var dojoconfig = {
+var dojoConfig = {
     baseUrl: '.',
     packages: [{
+        name: 'dgrid',
+        location: 'bower_components/dgrid',
+    }, {
+        name: 'dijit',
+        location: 'bower_components/dijit',
+    }, {
         name: 'dojo',
         location: 'bower_components/dojo',
+    }, {
+        name: 'dojox',
+        location: 'bower_components/dojox',
+    }, {
+        name: 'dstore',
+        location: 'bower_components/dstore',
+    }, {
+        name: 'esri',
+        location: 'bower_components/esri',
+    }, {
+        name: 'moment',
+        location: 'bower_components/moment',
     }, {
         name: 'react',
         location: 'node_modules/react/dist',
--- src/component/home.tsx
+++ src/component/home.tsx
@@ -1,11 +1,25 @@
 import * as React from "react";
 
+import Map = require("esri/Map");
+import MapView = require("esri/views/MapView");
+
 export interface HomeProps {}
 
 export class Home extends React.Component<HomeProps, {}> {
   render() {
+      var map = new Map({
+        basemap: "streets"
+      });
+
+      var view = new MapView({
+        container: "mapView",
+        map: map,
+        zoom: 4,
+        center: [15, 65]
+      });
     return (
       <div>
+      <div id="mapView" />
         <h1>This is Home!!!</h1>
         <h1>This is Home!!!</h1>
         <h1>This is Home!!!</h1>
